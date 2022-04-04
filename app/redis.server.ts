b3af1caf3b531b9c5b416d25810384c690603ad9
Andy Van Slaars <avanslaars@gmail.com>
2022-04-04 10:52:52 -0400

    super basic redis usage

---
+++ app/redis.server.ts
@@ -0,0 +1,29 @@
+import { createClient } from 'redis'
+
+let _client: any = null
+let _isConnected = false
+
+function createRedisClient() {
+  const client = createClient()
+  client.on('connect', () => {
+    _isConnected = true
+  })
+  client.on('end', () => {
+    _isConnected = false
+  })
+  return client
+}
+
+export async function getRedisClient() {
+  if (_client !== null) {
+    if (!_isConnected) {
+      await _client.connect()
+    }
+    return _client
+  }
+
+  const client = createRedisClient()
+  await client.connect()
+  _client = client
+  return _client
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,62 @@
+import { ActionFunction, LoaderFunction } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
+import { getRedisClient } from '~/redis.server'
+
+interface StuffItem {
+  id: string
+  name: string
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData.entries())
+  const redis = await getRedisClient()
+  await redis.hSet('stuff', data.key, data.name)
+
+  return null
+}
+
+export const loader: LoaderFunction = async () => {
+  let values: StuffItem[] = []
+  try {
+    const client = await getRedisClient()
+    const stuff = await client.hGetAll('stuff')
+    // console.log('stuff', stuff)
+
+    // convert to array
+    values = Object.keys(stuff).map((key) => {
+      return { id: key, name: stuff[key] }
+    })
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+  return { stuff: values }
+}
+
+// TODO: Deal with pending state and clearing the form
 export default function Index() {
+  const { stuff } = useLoaderData()
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
-      <form>
+      {/* The 405 status messed me up here. An index route posting to itself needs this ?index in the action or it won't work */}
+      <Form method="post" action="?index">
         <div>
-          <label htmlFor="key">Key</label>
+          <label htmlFor="key">Id</label>
           <input id="key" type="text" name="key" />
         </div>
         <div>
-          <label htmlFor="val">Value</label>
-          <input id="val" type="text" name="val" />
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" name="name" />
         </div>
         <button type="submit">Submit</button>
         <button type="reset">Reset</button>
-      </form>
+      </Form>
       <ul>
-        <li>items</li>
-        <li>from</li>
-        <li>redis</li>
-        <li>here</li>
+        {stuff.map(({ id, name }: StuffItem) => (
+          <li key={id}>{name}</li>
+        ))}
       </ul>
     </div>
   )
